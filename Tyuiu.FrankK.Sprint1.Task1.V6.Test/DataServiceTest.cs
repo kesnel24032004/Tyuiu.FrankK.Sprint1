11898a666ce58d2d0e94371a792b4f238b9e897f
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-26 18:33:07 +0500

    Task1

---
+++ Tyuiu.FrankK.Sprint1.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.FrankK.Sprint1.Task1.V6.Lib;
+
+namespace Tyuiu.FrankK.Sprint1.Task1.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3.0, 3.0);
+
+            Assert.AreEqual(0.6666666666666666, res);
+        }
+    }
+}
---
+++ Tyuiu.FrankK.Sprint1.Task1.V6/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.FrankK.Sprint1.Task1.V6.Lib;
+
+namespace Tyuiu.FrankK.Sprint1.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DataService ds = new DataService();
+            
+
+            double x, y;
+            Console.WriteLine("Введите x, через enter: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y, через enter: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.WriteLine(" РЕЗУЛЬТАТ:  ");
+            
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
